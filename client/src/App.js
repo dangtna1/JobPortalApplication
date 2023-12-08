00f90b3c07a9884127a255ac2a6a9dbbe69ba007
Dawn Vu <tamquattnb@gmail.com>
2023-12-08 22:46:31 +0700

    Perform CRUD on company table

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import CompanyTable from "./components/company/CompanyTable";
+// import CompanyTable from "./components/company/CompanyTable";
 import JobseekerTable from "./components/jobseeker/JobseekerTable";
 import Header from "./components/utils/Header";
 import Home from "./components/home/Home";
 import CompanyProfile from "./components/profile/CompanyProfile";
+import CompanyTable from "./components/company/CompanyTable";
+import CompanyForm from "./components/company/CompanyForm";
+import CompanyEdit from "./components/company/CompanyEdit";
 
 function App() {
   return (
     <div className="w-full">
       <Header />
       <div className="w-full flex mt-[15vh]">
-        {/* <Sidebar /> */}
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/company" element={<CompanyTable />} />
+          <Route path="/company/create" element={<CompanyForm />} />
+          <Route path="/company/edit/:id" element={<CompanyEdit />} />
           <Route path="/jobseeker" element={<JobseekerTable />} />
           <Route path="/profile/companies/:id" element={<CompanyProfile />} />
         </Routes>
       </div>
     </div>
-
   );
 }
 
--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,84 +1,185 @@
+import { Link } from "react-router-dom";
+
 export const companyData = {
-    columns: [
-        { field: 'id', headerName: 'ID', width: 100 },
-        { field: 'companyName', headerName: 'Company name', width: 200 },
-        { field: 'phoneNumber', headerName: 'Phone number', width: 200 },
-        {
-            field: 'industry',
-            headerName: 'Industry',
-            width: 200,
-        },
-        {
-            field: 'description',
-            headerName: 'Description',
-            description: 'This column is not sortable.',
-            sortable: false,
-            width: 200,
-        },
-        {
-            field: 'websiteUrl',
-            headerName: 'Website URL',
-            description: 'This column is not sortable.',
-            sortable: false,
-            width: 200,
-        },
-
-    ],
-
-    rows: [
-        { id: 1, companyName: 'A Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 2, companyName: 'B Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 3, companyName: 'C Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 4, companyName: 'D Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 5, companyName: 'E Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 6, companyName: 'F Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 7, companyName: 'G Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 8, companyName: 'H Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-        { id: 9, companyName: 'J Company', phoneNumber: '0123456789', industry: "IT", description: "No description", websiteUrl: "https://abc.com" },
-    ]
-
-}
-
+  columns: [
+    { field: "companyId", headerName: "ID", width: 50 },
+    {
+      field: "companyName",
+      headerName: "Company name",
+      width: 150,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 500,
+      renderCell: (params) => (
+        <div
+          style={{
+            whiteSpace: "pre-line",
+          }}
+        >
+          {params.value}
+        </div>
+      ),
+    },
+    {
+      field: "industry",
+      headerName: "Industry",
+      width: 100,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone number",
+      description: "This column is not sortable.",
+      sortable: false,
+      width: 150,
+    },
+    {
+      field: "websiteUrl",
+      headerName: "Website URL",
+      description: "This column is not sortable.",
+      sortable: false,
+      width: 350,
+      renderCell: (params) => (
+        <a href={params.value} target="_blank" rel="noopener noreferrer">
+          {params.value}
+        </a>
+      ),
+    },
+    {
+      field: "deleteButton",
+      headerName: "Actions",
+      width: 200,
+      renderCell: (params) => (
+        <div>
+          <Link
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            to={`/company/edit/${params.row.companyId}`}
+          >
+            Edit
+          </Link>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => console.log(params.row.companyId)}
+          >
+            Delete
+          </button>
+        </div>
+      ),
+    },
+  ],
+};
 
 export const jobseekerData = {
-    columns: [
-        { field: 'id', headerName: 'ID', width: 100 },
-        { field: 'fullName', headerName: 'Full Name', width: 200 },
-        { field: 'phoneNumber', headerName: 'Phone number', width: 200 },
-        {
-            field: 'note',
-            headerName: 'Note',
-            width: 200,
-        },
-        {
-            field: 'quote',
-            headerName: 'Quote',
-            description: 'This column is not sortable.',
-            sortable: false,
-            width: 200,
-        },
-        {
-            field: 'expSal',
-            headerName: 'Expected Salary',
-            width: 200,
-        },
-        {
-            field: 'lastAccessed',
-            headerName: "Last Accessed",
-            width: 200
-        }
+  columns: [
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "fullName", headerName: "Full Name", width: 200 },
+    { field: "phoneNumber", headerName: "Phone number", width: 200 },
+    {
+      field: "note",
+      headerName: "Note",
+      width: 200,
+    },
+    {
+      field: "quote",
+      headerName: "Quote",
+      description: "This column is not sortable.",
+      sortable: false,
+      width: 200,
+    },
+    {
+      field: "expSal",
+      headerName: "Expected Salary",
+      width: 200,
+    },
+    {
+      field: "lastAccessed",
+      headerName: "Last Accessed",
+      width: 200,
+    },
+  ],
 
-    ],
-
-    rows: [
-        { id: 1, fullName: 'Nguyen Van A', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 2, fullName: 'Nguyen Van B', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 3, fullName: 'Nguyen Van C', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 4, fullName: 'Nguyen Van D', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 5, fullName: 'Nguyen Van E', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 6, fullName: 'Nguyen Van F', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 7, fullName: 'Nguyen Van G', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 8, fullName: 'Nguyen Van H', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-        { id: 9, fullName: 'Nguyen Van J', phoneNumber: '0123456789', note: "No note", quote: "No quote", expSal: "5000$", lastAccessed: "a day ago" },
-    ]
-}
+  rows: [
+    {
+      id: 1,
+      fullName: "Nguyen Van A",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 2,
+      fullName: "Nguyen Van B",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 3,
+      fullName: "Nguyen Van C",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 4,
+      fullName: "Nguyen Van D",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 5,
+      fullName: "Nguyen Van E",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 6,
+      fullName: "Nguyen Van F",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 7,
+      fullName: "Nguyen Van G",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 8,
+      fullName: "Nguyen Van H",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+    {
+      id: 9,
+      fullName: "Nguyen Van J",
+      phoneNumber: "0123456789",
+      note: "No note",
+      quote: "No quote",
+      expSal: "5000$",
+      lastAccessed: "a day ago",
+    },
+  ],
+};
