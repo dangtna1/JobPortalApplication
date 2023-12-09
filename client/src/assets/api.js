299a7208990debfe7e84c2dec12f22054b22525b
Dawn Vu <tamquattnb@gmail.com>
2023-12-09 20:39:49 +0700

    Finish company page

--- client/src/assets/api.js
+++ client/src/assets/api.js
@@ -1,35 +1,55 @@
 export const fetchJobList = async () => {
-    try {
+  try {
+    const res = await fetch("api/job/");
 
-        const res = await fetch("api/job/");
-
-        if (!res.ok) {
-            throw new Error("Network response was not ok");
-        }
-
-        const data = await res.json();
-        return data;
-
-    } catch (err) {
-        console.log(err);
-        throw (err);
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
     }
-}
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
 export const searchJob = async ({ keyword, salary }) => {
-    try {
+  try {
+    const res = await fetch(
+      `api/job/search?keyword=${keyword}&salary=${salary}`
+    );
 
-        const res = await fetch(`api/job/search?keyword=${keyword}&salary=${salary}`);
-
-        if (!res.ok) {
-            throw new Error("Network response was not ok");
-        }
-
-        const data = await res.json();
-        return data;
-
-    } catch (err) {
-        console.log(err);
-        throw (err);
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
     }
-}
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const searchCompany = async ({ keyword, salary }) => {
+  //   console.log(
+  //     `/api/company/search-by-salary?keyword=${keyword}&salary=${salary}`
+  //   );
+  try {
+    const res = await fetch(
+      `/api/company/search-by-salary?keyword=${keyword}&salary=${salary}`
+    );
+
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
